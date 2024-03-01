eb14a589c1e97d9943cb8ed9ba3d5e0737ddf2ee
ivan <falso@falso.es>
2024-03-01 13:13:00 +0100

    He quitado la anchura y altura de la ventana al ejecutar en application, ahora he creado una alerta en el controller

--- src/main/java/com/example/anchorpane/HelloController.java
+++ src/main/java/com/example/anchorpane/HelloController.java
@@ -3,6 +3,7 @@ package com.example.anchorpane;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Alert;
 
 public class HelloController {
 
@@ -14,6 +15,12 @@ public class HelloController {
 
     @FXML
     protected void onEnviar() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Información");
+        alert.setHeaderText(null);
+        alert.setContentText("¡¡ALERTA, ALERTA!! NOS ATACAN!");
+        alert.showAndWait();
+
         if (confirmacion.isSelected()){
             informacion.setText("El usuario acepta las condiciones");
         } else {
